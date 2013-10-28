6940d707799db7ae831469576f6286e73f0b6b7f
Fab1en <Fab1en@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-28 14:59:50 +0000

    [fix] Remove wp_load_image use (deprecated)
    git-svn-id: http://plugins.svn.wordpress.org/grayscale/trunk@794839 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class-grayscale-image-editor.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once(ABSPATH . 'wp-includes/class-wp-image-editor.php');
+require_once(ABSPATH . 'wp-includes/class-wp-image-editor-gd.php');
+
+class Grayscale_Image_Editor extends WP_Image_Editor_GD {
+    public function make_grayscale(){
+        // create a copy
+        $dest = $this->image;
+        unset($this->image);
+        $this->load();
+        if ( is_resource( $dest ) ) {
+            // Apply grayscale filter
+            imagecopymergegray($dest, $this->image, 0, 0, 0, 0, $this->size['width'], $this->size['height'], 0);
+            imagegammacorrect($dest, 1.0, 0.7);
+            imagedestroy( $this->image );
+            $this->image = $dest;
+            return true;
+        }
+    }
+}
