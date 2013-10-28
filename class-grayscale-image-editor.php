982a08e86d5e8f3ec68ea692b731032294d76d61
Fabien Quatravaux <fabien.quatravaux@1nterval.com>
2013-10-28 15:47:58 +0100

    [dev] Add a filter to tune gamma correction

--- class-grayscale-image-editor.php
+++ class-grayscale-image-editor.php
@@ -12,7 +12,7 @@ class Grayscale_Image_Editor extends WP_Image_Editor_GD {
         if ( is_resource( $dest ) ) {
             // Apply grayscale filter
             imagecopymergegray($dest, $this->image, 0, 0, 0, 0, $this->size['width'], $this->size['height'], 0);
-            imagegammacorrect($dest, 1.0, 0.7);
+            imagegammacorrect($dest, 1.0, apply_filters('grayscale_gamma_correction', 0.7));
             imagedestroy( $this->image );
             $this->image = $dest;
             return true;
